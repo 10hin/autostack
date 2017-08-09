f0c2b77661e4afc9e118457560df41bf8ce533df
unknown <>
2017-08-10 00:46:59 +0900

    StackStream実装中

--- src/main/java/autostack/StackStream.java
+++ src/main/java/autostack/StackStream.java
@@ -50,26 +50,22 @@ public class StackStream<H, R extends AutoStack<?, ?>> implements Stream<H> {
 
     @Override
     public StackStream<H, R> sequential() {
-        // TODO 自動生成されたメソッド・スタブ
-        return this;
+        return new StackStream<>(this.stream.sequential());
     }
 
     @Override
     public StackStream<H, R> parallel() {
-        // TODO 自動生成されたメソッド・スタブ
-        return this;
+        return new StackStream<>(this.stream.parallel());
     }
 
     @Override
     public StackStream<H, R> unordered() {
-        // TODO 自動生成されたメソッド・スタブ
-        return this;
+        return new StackStream<H, R>(this.stream.unordered());
     }
 
     @Override
-    public Stream<H> onClose(Runnable closeHandler) {
-        this.stream.onClose(closeHandler);
-        return this;
+    public StackStream<H, R> onClose(Runnable closeHandler) {
+        return new StackStream<H, R>(this.stream.onClose(closeHandler));
     }
 
     @Override
@@ -124,7 +120,7 @@ public class StackStream<H, R extends AutoStack<?, ?>> implements Stream<H> {
                 Stream<? extends T> mapped = mapper.apply(head);
                 R rest = stk.pop();
                 @SuppressWarnings("unchecked")
-                Stream<AutoStack<T, R>> ret = mapped.map(e -> rest.push((T) e));
+                Stream<AutoStack<T, R>> ret = mapped.map(e -> (AutoStack<T, R>) rest.push((T) e));
                 return ret;
             }
         }));
@@ -139,155 +135,169 @@ public class StackStream<H, R extends AutoStack<?, ?>> implements Stream<H> {
     @Override
     public LongStream flatMapToLong(Function<? super H, ? extends LongStream> mapper) {
         // TODO 自動生成されたメソッド・スタブ
-        return null;
+        throw new UnsupportedOperationException("unsupported");
     }
 
     @Override
     public DoubleStream flatMapToDouble(Function<? super H, ? extends DoubleStream> mapper) {
         // TODO 自動生成されたメソッド・スタブ
-        return null;
+        throw new UnsupportedOperationException("unsupported");
     }
 
     @Override
     public Stream<H> distinct() {
         // TODO 自動生成されたメソッド・スタブ
-        return null;
+        throw new UnsupportedOperationException("unsupported");
     }
 
     @Override
-    public Stream<H> sorted() {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+    public StackStream<H, R> sorted() {
+        return new StackStream<>(this.stream.sorted(new Comparator<AutoStack<H, R>>() {
+            @Override
+            public int compare(AutoStack<H, R> o1, AutoStack<H, R> o2) {
+                @SuppressWarnings("unchecked")
+                Comparable<H> c1 = (Comparable<H>) o1.head();
+                H h2 = o2.head();
+                return c1.compareTo(h2);
+            }
+        }));
     }
 
     @Override
     public Stream<H> sorted(Comparator<? super H> comparator) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return new StackStream<>(this.stream.sorted(Comparator.comparing(AutoStack<H, R>::head, comparator)));
     }
 
     @Override
-    public Stream<H> peek(Consumer<? super H> action) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+    public StackStream<H, R> peek(Consumer<? super H> action) {
+        return new StackStream<>(this.stream.peek(new Consumer<AutoStack<H, R>>() {
+            @Override
+            public void accept(AutoStack<H, R> stk) {
+                action.accept(stk.head());
+            }
+        }));
     }
 
     @Override
     public Stream<H> limit(long maxSize) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return new StackStream<>(this.stream.limit(maxSize));
     }
 
     @Override
     public Stream<H> skip(long n) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return new StackStream<>(this.stream.skip(n));
     }
 
     @Override
     public void forEach(Consumer<? super H> action) {
-        // TODO 自動生成されたメソッド・スタブ
-        
+        this.stream.forEach(new Consumer<AutoStack<H, R>>() {
+            @Override
+            public void accept(AutoStack<H, R> stk) {
+                action.accept(stk.head());
+            }
+        });
     }
 
     @Override
     public void forEachOrdered(Consumer<? super H> action) {
-        // TODO 自動生成されたメソッド・スタブ
-        
+        this.stream.forEachOrdered(new Consumer<AutoStack<H, R>>() {
+            @Override
+            public void accept(AutoStack<H, R> stk) {
+                action.accept(stk.head());
+            }
+        });
     }
 
     @Override
     public Object[] toArray() {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return this.stream.map(AutoStack<H, R>::head).toArray();
     }
 
     @Override
     public <A> A[] toArray(IntFunction<A[]> generator) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return this.stream.map(AutoStack<H, R>::head).toArray(generator);
     }
 
     @Override
     public H reduce(H identity, BinaryOperator<H> accumulator) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return this.stream.map(AutoStack<H, R>::head).reduce(identity, accumulator);
     }
 
     @Override
     public Optional<H> reduce(BinaryOperator<H> accumulator) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return this.stream.map(AutoStack<H, R>::head).reduce(accumulator);
     }
 
     @Override
     public <U> U reduce(U identity, BiFunction<U, ? super H, U> accumulator, BinaryOperator<U> combiner) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return this.stream.map(AutoStack<H, R>::head).reduce(identity, accumulator, combiner);
     }
 
     @Override
-    public <R> R collect(Supplier<R> supplier, BiConsumer<R, ? super H> accumulator, BiConsumer<R, R> combiner) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+    public <T> T collect(Supplier<T> supplier, BiConsumer<T, ? super H> accumulator, BiConsumer<T, T> combiner) {
+        return this.stream.map(AutoStack<H, R>::head).collect(supplier, accumulator, combiner);
     }
 
     @Override
-    public <R, A> R collect(Collector<? super H, A, R> collector) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+    public <T, A> T collect(Collector<? super H, A, T> collector) {
+        return this.stream.map(AutoStack<H, R>::head).collect(collector);
     }
 
     @Override
     public Optional<H> min(Comparator<? super H> comparator) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return this.stream.map(AutoStack<H, R>::head).min(comparator);
     }
 
     @Override
     public Optional<H> max(Comparator<? super H> comparator) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return this.stream.map(AutoStack<H, R>::head).max(comparator);
     }
 
     @Override
     public long count() {
-        // TODO 自動生成されたメソッド・スタブ
-        return 0;
+        return this.stream.map(AutoStack<H, R>::head).count();
     }
 
     @Override
     public boolean anyMatch(Predicate<? super H> predicate) {
-        // TODO 自動生成されたメソッド・スタブ
-        return false;
+        return this.stream.map(AutoStack<H, R>::head).anyMatch(predicate);
     }
 
     @Override
     public boolean allMatch(Predicate<? super H> predicate) {
-        // TODO 自動生成されたメソッド・スタブ
-        return false;
+        return this.stream.map(AutoStack<H, R>::head).allMatch(predicate);
     }
 
     @Override
     public boolean noneMatch(Predicate<? super H> predicate) {
-        // TODO 自動生成されたメソッド・スタブ
-        return false;
+        return this.stream.map(AutoStack<H, R>::head).noneMatch(predicate);
     }
 
     @Override
     public Optional<H> findFirst() {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return this.stream.map(AutoStack<H, R>::head).findFirst();
     }
 
     @Override
     public Optional<H> findAny() {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        return this.stream.map(AutoStack<H, R>::head).findAny();
     }
 
-    public static <T> Collector<T, ?, StackStream<T, ?>> stacking() {
-        return Collector.of(StackStream.<T>empty(), null, null);
+    public static <H, R extends AutoStack<?, ?>> Collector<H, ?, StackStream<H, R>> stacking() {
+        return Collector.of(StackStream::empty, new BiConsumer<StackStream<H, R>, H>() {
+            @Override
+            public void accept(StackStream<H, R> stkStream, H head) {
+            }
+        }, new BinaryOperator<StackStream<H, R>>() {
+            @Override
+            public StackStream<H, R> apply(StackStream<H, R> stkStream1, StackStream<H, R> stkStream2) {
+                return new StackStream<>(Stream.concat(stkStream1.stream, stkStream2.stream));
+            }
+        });
+    }
+
+    public static <H, R extends AutoStack<?, ?>> StackStream<H, R> empty() {
+        return new StackStream<>();
     }
 
 }
